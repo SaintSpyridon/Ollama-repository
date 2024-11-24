ollama/ollama

Add support for v5 and v6+ multi-gpu cuda setups
With the new addition of cuda v12 and selecting the newer version for GPUs
that support it, users with a mix of v11 only and v12 supporting GPUs weren't
able to load models spanning the GPU.  This refines the algorithm to use the oldest
variant (v11 in this case) so that all the GPUs will be supported.  This will break
down if a user has a CC 5.x card and a CC 9.x card, as we don't currently
have plumbing in place to understand the maximum version support.  The Go
server will open up the ability to expose a more refined support matrix from
the runners, and we can refine this algorithm then.

runners/common.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"runtime"
 	"slices"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -300,12 +301,18 @@ func GetAvailableServers(payloadsDir string) map[string]string {
 
 // serversForGpu returns a list of compatible servers give the provided GPU
 // info, ordered by performance. assumes Init() has been called
-// TODO - switch to metadata based mapping
-func ServersForGpu(info gpu.GpuInfo) []string {
+// If multiple variants are present, oldest is chosen
+func ServersForGpu(gl []gpu.GpuInfo) []string {
 	// glob workDir for files that start with ollama_
 	availableServers := GetAvailableServers(runnersDir)
+
+	// Sort by variant, which will yield the oldest variant first
+	sgl := append(make(gpu.GpuInfoList, 0, len(gl)), gl...)
+	sort.Sort(gpu.ByVariant(sgl))
+	info := sgl[0]
+
 	requested := info.Library
-	if info.Variant != gpu.CPUCapabilityNone.String() {
+	if info.Variant != "" && info.Variant != gpu.CPUCapabilityNone.String() {
 		requested += "_" + info.Variant
 	}
 

runners/runners_test.go
@@ -1,22 +1,38 @@
 package runners
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 	"path"
 	"runtime"
 	"strings"
+	"sync"
 	"testing"
 	"testing/fstest"
+
+	"github.com/ollama/ollama/gpu"
 )
+
+var runnerTmpDir = sync.OnceValues(func() (string, error) {
+	tmpDir, err := os.MkdirTemp("", "testing")
+	if err != nil {
+		return "", fmt.Errorf("failed to make tmp dir %w", err)
+	}
+	return tmpDir, nil
+})
 
 func TestRefreshRunners(t *testing.T) {
 	slog.SetLogLoggerLevel(slog.LevelDebug)
 
 	payloadFS := fstest.MapFS{
-		path.Join(runtime.GOOS, runtime.GOARCH, "foo", "ollama_llama_server"): {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "cpu", "ollama_llama_server"):     {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "cpu_avx", "ollama_llama_server"): {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "foo_v12", "ollama_llama_server"): {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "foo_v11", "ollama_llama_server"): {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "bar", "ollama_llama_server"):     {Data: []byte("hello, world\n")},
 	}
-	tmpDir, err := os.MkdirTemp("", "testing")
+	tmpDir, err := runnerTmpDir()
 	if err != nil {
 		t.Fatalf("failed to make tmp dir %s", err)
 	}
@@ -31,6 +47,7 @@ func TestRefreshRunners(t *testing.T) {
 
 	// spot check results
 	servers := GetAvailableServers(rDir)
+	slog.Debug("GetAvailableServers", "response", servers)
 	if len(servers) < 1 {
 		t.Fatalf("expected at least 1 server")
 	}
@@ -42,6 +59,42 @@ func TestRefreshRunners(t *testing.T) {
 	}
 	if !strings.Contains(rDir, tmpDir) {
 		t.Fatalf("runner dir %s was not in tmp dir %s", rDir, tmpDir)
+	}
+
+	// Verify correct server response list
+	type testCase struct {
+		input  []gpu.GpuInfo
+		expect []string
+	}
+	testCases := map[string]*testCase{
+		"cpu":     {input: []gpu.GpuInfo{{Library: "cpu"}}, expect: []string{"cpu"}},
+		"cpu_avx": {input: []gpu.GpuInfo{{Library: "cpu", Variant: "avx"}}, expect: []string{"cpu_avx"}},
+	}
+	if runtime.GOOS == "darwin" && runtime.GOARCH == "arm64" {
+		// macos arm doesn't append "cpu"
+		testCases["foo no variant"] = &testCase{input: []gpu.GpuInfo{{Library: "foo"}}, expect: []string{"foo_v11", "foo_v12"}}
+		testCases["foo v11"] = &testCase{input: []gpu.GpuInfo{{Library: "foo", Variant: "v11"}}, expect: []string{"foo_v11", "foo_v12"}}
+		testCases["foo v12"] = &testCase{input: []gpu.GpuInfo{{Library: "foo", Variant: "v12"}}, expect: []string{"foo_v12", "foo_v11"}}
+		testCases["foo v11 and v12"] = &testCase{input: []gpu.GpuInfo{{Library: "foo", Variant: "v11"}, {Library: "foo", Variant: "v12"}}, expect: []string{"foo_v11", "foo_v12"}}
+	} else {
+		// All other platforms append "cpu" as a last ditch runner
+		testCases["foo no variant"] = &testCase{input: []gpu.GpuInfo{{Library: "foo"}}, expect: []string{"foo_v11", "foo_v12", "cpu"}}
+		testCases["foo v11"] = &testCase{input: []gpu.GpuInfo{{Library: "foo", Variant: "v11"}}, expect: []string{"foo_v11", "foo_v12", "cpu"}}
+		testCases["foo v12"] = &testCase{input: []gpu.GpuInfo{{Library: "foo", Variant: "v12"}}, expect: []string{"foo_v12", "foo_v11", "cpu"}}
+		testCases["foo v11 and v12"] = &testCase{input: []gpu.GpuInfo{{Library: "foo", Variant: "v11"}, {Library: "foo", Variant: "v12"}}, expect: []string{"foo_v11", "foo_v12", "cpu"}}
+	}
+	for k, v := range testCases {
+		t.Run(k, func(t *testing.T) {
+			resp := ServersForGpu(v.input)
+			if len(resp) != len(v.expect) {
+				t.Fatalf("expected length %d, got %d => %+v", len(v.expect), len(resp), resp)
+			}
+			for i := range resp {
+				if resp[i] != v.expect[i] {
+					t.Fatalf("expected offset %d, got %s wanted %s", i, resp[i], v.expect[i])
+				}
+			}
+		})
 	}
 
 	cleanupTmpDirs()

SaintSpyridon/Ollama-repository

runners/common.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"runtime"
 	"slices"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -300,12 +301,18 @@ func GetAvailableServers(payloadsDir string) map[string]string {
 
 // serversForGpu returns a list of compatible servers give the provided GPU
 // info, ordered by performance. assumes Init() has been called
-// TODO - switch to metadata based mapping
-func ServersForGpu(info discover.GpuInfo) []string {
+// If multiple variants are present, oldest is chosen
+func ServersForGpu(gl []discover.GpuInfo) []string {
 	// glob workDir for files that start with ollama_
 	availableServers := GetAvailableServers(runnersDir)
+
+	// Sort by variant, which will yield the oldest variant first
+	sgl := append(make(discover.GpuInfoList, 0, len(gl)), gl...)
+	sort.Sort(discover.ByVariant(sgl))
+	info := sgl[0]
+
 	requested := info.Library
-	if info.Variant != discover.CPUCapabilityNone.String() {
+	if info.Variant != "" && info.Variant != discover.CPUCapabilityNone.String() {
 		requested += "_" + info.Variant
 	}
 
@@ -353,6 +360,7 @@ func ServersForGpu(info discover.GpuInfo) []string {
 						break
 					}
 				}
+				servers = append(servers, "cpu")
 			} else {
 				servers = append(servers, "cpu")
 			}

runners/runners_test.go
@@ -1,22 +1,38 @@
 package runners
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 	"path"
 	"runtime"
 	"strings"
+	"sync"
 	"testing"
 	"testing/fstest"
+
+	"github.com/ollama/ollama/discover"
 )
+
+var runnerTmpDir = sync.OnceValues(func() (string, error) {
+	tmpDir, err := os.MkdirTemp("", "testing")
+	if err != nil {
+		return "", fmt.Errorf("failed to make tmp dir %w", err)
+	}
+	return tmpDir, nil
+})
 
 func TestRefreshRunners(t *testing.T) {
 	slog.SetLogLoggerLevel(slog.LevelDebug)
 
 	payloadFS := fstest.MapFS{
-		path.Join(runtime.GOOS, runtime.GOARCH, "foo", "ollama_llama_server"): {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "cpu", "ollama_llama_server"):     {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "cpu_avx", "ollama_llama_server"): {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "foo_v12", "ollama_llama_server"): {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "foo_v11", "ollama_llama_server"): {Data: []byte("hello, world\n")},
+		path.Join(runtime.GOOS, runtime.GOARCH, "bar", "ollama_llama_server"):     {Data: []byte("hello, world\n")},
 	}
-	tmpDir, err := os.MkdirTemp("", "testing")
+	tmpDir, err := runnerTmpDir()
 	if err != nil {
 		t.Fatalf("failed to make tmp dir %s", err)
 	}
@@ -31,6 +47,7 @@ func TestRefreshRunners(t *testing.T) {
 
 	// spot check results
 	servers := GetAvailableServers(rDir)
+	slog.Debug("GetAvailableServers", "response", servers)
 	if len(servers) < 1 {
 		t.Fatalf("expected at least 1 server")
 	}
@@ -42,6 +59,42 @@ func TestRefreshRunners(t *testing.T) {
 	}
 	if !strings.Contains(rDir, tmpDir) {
 		t.Fatalf("runner dir %s was not in tmp dir %s", rDir, tmpDir)
+	}
+
+	// Verify correct server response list
+	type testCase struct {
+		input  []discover.GpuInfo
+		expect []string
+	}
+	testCases := map[string]*testCase{
+		"cpu":     {input: []discover.GpuInfo{{Library: "cpu"}}, expect: []string{"cpu"}},
+		"cpu_avx": {input: []discover.GpuInfo{{Library: "cpu", Variant: "avx"}}, expect: []string{"cpu_avx"}},
+	}
+	if runtime.GOOS == "darwin" && runtime.GOARCH == "arm64" {
+		// macos arm doesn't append "cpu"
+		testCases["foo no variant"] = &testCase{input: []discover.GpuInfo{{Library: "foo"}}, expect: []string{"foo_v11", "foo_v12"}}
+		testCases["foo v11"] = &testCase{input: []discover.GpuInfo{{Library: "foo", Variant: "v11"}}, expect: []string{"foo_v11", "foo_v12"}}
+		testCases["foo v12"] = &testCase{input: []discover.GpuInfo{{Library: "foo", Variant: "v12"}}, expect: []string{"foo_v12", "foo_v11"}}
+		testCases["foo v11 and v12"] = &testCase{input: []discover.GpuInfo{{Library: "foo", Variant: "v11"}, {Library: "foo", Variant: "v12"}}, expect: []string{"foo_v11", "foo_v12"}}
+	} else {
+		// All other platforms append "cpu" as a last ditch runner
+		testCases["foo no variant"] = &testCase{input: []discover.GpuInfo{{Library: "foo"}}, expect: []string{"foo_v11", "foo_v12", "cpu"}}
+		testCases["foo v11"] = &testCase{input: []discover.GpuInfo{{Library: "foo", Variant: "v11"}}, expect: []string{"foo_v11", "foo_v12", "cpu"}}
+		testCases["foo v12"] = &testCase{input: []discover.GpuInfo{{Library: "foo", Variant: "v12"}}, expect: []string{"foo_v12", "foo_v11", "cpu"}}
+		testCases["foo v11 and v12"] = &testCase{input: []discover.GpuInfo{{Library: "foo", Variant: "v11"}, {Library: "foo", Variant: "v12"}}, expect: []string{"foo_v11", "foo_v12", "cpu"}}
+	}
+	for k, v := range testCases {
+		t.Run(k, func(t *testing.T) {
+			resp := ServersForGpu(v.input)
+			if len(resp) != len(v.expect) {
+				t.Fatalf("expected length %d, got %d => %+v", len(v.expect), len(resp), resp)
+			}
+			for i := range resp {
+				if resp[i] != v.expect[i] {
+					t.Fatalf("expected offset %d, got %s wanted %s", i, resp[i], v.expect[i])
+				}
+			}
+		})
 	}
 
 	cleanupTmpDirs()